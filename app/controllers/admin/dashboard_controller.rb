b4b324823d8464972c27dc9e4c3362e73e208450
Robert <rbbenett0614@gmail.com>
2021-02-03 21:15:18 +0000

    Updated authentication keys

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,8 @@
 class Admin::DashboardController < ApplicationController
 
-  http_basic_authenticate_with name: ENV["HTTP_USERNAME"].to_s,
-                               password: ENV["HTTP_ACCESS_KEY"].to_s
-                               
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"].to_s,
+                               password: ENV["ADMIN_ACCESS_KEY"].to_s
+
   def show
   end
 end
