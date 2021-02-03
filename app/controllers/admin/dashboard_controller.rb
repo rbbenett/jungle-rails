c55053b2e86dd94971433b37ce941c62ec6a442d
Robert <rbbenett0614@gmail.com>
2021-02-03 22:00:34 +0000

    Added counts of items and categories to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,5 +4,7 @@ class Admin::DashboardController < ApplicationController
                                password: ENV["ADMIN_ACCESS_KEY"].to_s
 
   def show
+    @products = Product.all.count
+    @categories = Category.all.count
   end
 end
