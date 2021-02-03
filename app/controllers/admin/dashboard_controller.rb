3e0c42a752598a8854b768c6a08e3525bc1f0951
Robert <rbbenett0614@gmail.com>
2021-02-03 21:13:50 +0000

    Added http authentication to admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["HTTP_USERNAME"].to_s,
+                               password: ENV["HTTP_ACCESS_KEY"].to_s
+                               
   def show
   end
 end
