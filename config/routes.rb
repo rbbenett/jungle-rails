4c1a0f8cedaa2ae8b32d06a2e017ff1ec4c53863
Robert <rbbenett0614@gmail.com>
2021-02-04 17:23:53 +0000

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,13 +19,6 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
-   # I've created a gif controller so I have a page I can secure later. 
-   # This is optional (as is the root to: above).
-   get '/cool' => 'gif#cool'
-   get '/sweet' => 'gif#sweet'
- 
-   # These routes will be for signup. The first renders a form in the browse, the second will 
-   # receive the form and create a user in our database using the data given to us by the user.
    get '/signup' => 'users#new'
    post '/users' => 'users#create'
 
